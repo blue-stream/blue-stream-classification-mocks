8be843a6b3657d7615a9abfbf166b8216bd69ef1
Yuval Assa <assa.yuval107@gmail.com>
2019-01-22 15:37:02 +0200

    feat: Create authenticator

---
+++ authenticator.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const passport = require("passport");
+const passportJwt = require("passport-jwt");
+class Authenticator {
+    static initialize(verifyCallback) {
+        const strategy = new passportJwt.Strategy(Authenticator.jwtOptions, function (jwtPayload, next) {
+            if (verifyCallback) {
+                verifyCallback(jwtPayload, next);
+            }
+            else {
+                next(null, jwtPayload);
+            }
+        });
+        passport.use(strategy);
+        return passport.initialize();
+    }
+    static middleware(req, res, next) {
+        passport.authenticate('jwt', { session: false })(req, res, next);
+    }
+}
+Authenticator.jwtOptions = {
+    jwtFromRequest: passportJwt.ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'bLue5tream@classiFiCaToR',
+};
+exports.Authenticator = Authenticator;
+//# sourceMappingURL=authenticator.js.map
